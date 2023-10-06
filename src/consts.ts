fcc21127c0aeb2c68c6480038298f645a9dd4626
Moray Macdonald <moray.macdonald1@gmail.com>
2023-10-06 21:48:02 +0100

    Add website content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Capability Charlotte - Wiltshire-based wildlife friendly gardens';
+export const SITE_DESCRIPTION = 'Award-winning garden designer based near Bath. Creating natural gardens bursting with biodiversity from beautiful, hand-drawn designs.';

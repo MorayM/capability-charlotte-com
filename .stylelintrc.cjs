3abcf9f3a3a6d8b9c3293ad84c68a5e2d737f26a
Moray Macdonald <moray.macdonald1@gmail.com>
2023-10-11 09:47:47 +0100

    Add lint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "stylelint-config-html/html",
+    "stylelint-config-html/xml",
+    "stylelint-config-html/astro",
+  ],
+  ignoreFiles: ["dist"],
+  rules: {},
+};

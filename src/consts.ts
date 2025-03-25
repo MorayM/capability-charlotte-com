c4cd537628a1e89f719659157f6ba8c1a56d2ebe
Moray Macdonald <moray.macdonald1@gmail.com>
2025-03-25 22:18:31 +0000

    Update tags and headers

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Capability Charlotte - Wiltshire-based wildlife friendly gardens';
-export const SITE_DESCRIPTION = 'Award-winning garden designer based near Bath. Creating natural gardens bursting with biodiversity from beautiful, hand-drawn designs.';
+export const SITE_DESCRIPTION = 'Award-winning garden designer in Wiltshire creating wildlife-friendly, hand-drawn natural gardens that burst with biodiversity. Discover sustainable garden design that transforms your outdoor space.';
